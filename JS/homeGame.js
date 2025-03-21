8e27c52b3817144a8b998a7fcddc6e45aadd11d5
Ahmed Ali <universalme.mss@gmail.com>
2025-03-22 02:16:01 +0500

    Add files via upload

---
+++ JS/homeGame.js
@@ -0,0 +1,43 @@
+document.title = "Street Warriors: Karachi";
+
+
+document.body.innerHTML += `
+    <video 
+        src="Videos/videoplayback (4).mp4" 
+        autoplay 
+        loop 
+        muted 
+        style="
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100vw;
+            height: 100vh;
+            object-fit: cover;
+            z-index: -1;
+        "
+    ></video>
+`;
+
+// ************************************************************************** //
+
+let audio = document.getElementById("backgroundAudio");
+let button = document.getElementById("toggleButton");
+
+
+button.addEventListener("click", function() {
+    if (audio.muted) {
+        audio.muted = false;
+        button.innerText = "🔇 Mute";
+    } else {
+        audio.muted = true;
+        button.innerText = "🔊 Play";
+    }
+
+
+    if (audio.paused) {
+        audio.play();
+    }
+});
+
+
